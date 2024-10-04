a6973efe0d85d0351d25b1b9b81ba6f1efa69db7
Dilrabo1991 <dilraboxujamuratova@gmail.com>
2024-10-04 11:29:43 +0500

    new changes in form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,53 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import './Form.css'
-import { useTelegram } from '../Hooks/useTelegram'
+import React, { useEffect, useState } from 'react';
+import './Form.css';
+import { useTelegram } from '../Hooks/useTelegram';
+
 function Form() {
+  const [country, setCountry] = useState('');
+  const [street, setStreet] = useState('');
+  const [subgact, setSubgact] = useState('physical');
+  const { tg } = useTelegram();
 
-const [country, setCountry] = useState('')
-const [street, setStreet] = useState('')
-const [subgact, setSubgact] = useState('physical')
-const {tg} =useTelegram()
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
+  };
 
-const onChangeCountry = (e) => {
-    setCountry(e.target.value)
-}
-const onChangeCity = (e) => {
-    setStreet(e.target.value)
-}
-const onChangeSubgact = (e) => {
-    setSubgact(e.target.value)
-}
+  const onChangeStreet = (e) => {
+    setStreet(e.target.value);
+  };
 
+  const onChangeSubgact = (e) => {
+    setSubgact(e.target.value);
+  };
 
-useEffect(() => {	
-tg.Main.Button.SetParams({
-    text: 'Malumotlarni Yuborish'
-})
-}, [])
+  // Set MainButton text
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: 'Malumotlarni Yuborish',
+    });
+  }, [tg]);
 
-useEffect(() => {	
-   if(!street || !country) {
-    tg.MainButton.hide()
-   } else {
-    tg.MainButton.show()
-   }
-   
-    }, [country, street, ])
+  // Show/hide MainButton based on input validity
+  useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+    }
+  }, [country, street, tg]);
 
   return (
-    <div className='form'>
-     <h3>Malumotlaringizni kirgizing</h3>
-     <input type="text" className='input' placeholder="Shahar"
-     value={country} onChange={onChangeCountry} />
-     <input type="text" className='input' placeholder="Koçha"
-     value={street} onChange={onChangeCity} />
-  
-     <select className='select'    value={subgact} onChange={onChangeSubgact}>
-       <option value={'physical'}>Jismoniy Shahs</option>
-       <option value={'legal'}>Yuridik Shahs</option>
-       </select>
+    <div className="form">
+      <h3>Malumotlaringizni kirgizing</h3>
+      <input
+        type="text"
+        className="input"
+        placeholder="Shahar"
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input
+        type="text"
+        className="input"
+        placeholder="Ko'cha"
+        value={street}
+        onChange={onChangeStreet}
+      />
+
+      <select className="select" value={subgact} onChange={onChangeSubgact}>
+        <option value="physical">Jismoniy Shahs</option>
+        <option value="legal">Yuridik Shahs</option>
+      </select>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
