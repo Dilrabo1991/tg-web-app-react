d594655350123d0d2c11a12032d06157d0e9f8bd
Dilrabo1991 <dilraboxujamuratova@gmail.com>
2024-10-04 10:25:44 +0500

    new change in today

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Button.css';
 function Button({ className, ...props }) {
   return (
     <button {...props} className={`button ${className ? className : ''}`}>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useTelegram } from '../Hooks/useTelegram'
 import Button from '../Button/Button' 
+import './Header.css'
 function Header() {
     const { onClose, user}= useTelegram()
   
